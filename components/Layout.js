05199453a1ca378382381bd0ff40991b67aad83f
Pablo Varela <pablovarela182@gmail.com>
2018-02-13 23:36:53 +0100

    Add analytics

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,31 @@
+import React from 'react'
 import Head from 'next/head'
+import ReactGA from 'react-ga'
 
-export default ({ children }) =>
-  <div>
-    <Head>
-      <title>Vim Colors</title>
-      <meta
-        name='viewport'
-        content='width=device-width, user-scalable=no'
-      />
-    </Head>
-    <main>{ children }</main>
-    <style jsx>{`
-      main {
-        max-width: 1000px;
-        margin: 0 auto;
-        padding: 1em;
-        font-family: 'SF Mono', Menlo, monospace;
-      }
-    `}</style>
-  </div>
+export default class extends React.Component {
+  componentDidMount () {
+    ReactGA.initialize('UA-106008527-4')
+    ReactGA.pageview(document.location.pathname)
+  }
+
+  render () {
+    return <div>
+      <Head>
+        <title>Vim Colors</title>
+        <meta
+          name='viewport'
+          content='width=device-width, user-scalable=no'
+        />
+      </Head>
+      <main>{ this.props.children }</main>
+      <style jsx>{`
+        main {
+          max-width: 1000px;
+          margin: 0 auto;
+          padding: 1em;
+          font-family: 'SF Mono', Menlo, monospace;
+        }
+      `}</style>
+    </div>
+  }
+}
