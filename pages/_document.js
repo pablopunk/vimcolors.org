accf6f199684bdd840e44fc0cffcafb22b156758
pablopunk <pablovarela182@gmail.com>
2019-05-26 17:08:02 +0200

    open graph

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,12 @@ export default class MyDocument extends Document {
           <meta name='description' content='Generate your custom colorscheme for vim and export it to a vim-compatible file' />
           <meta name='keywords' content='vim,colors,scheme,colorscheme,generator,generate,website,modern,minimalistic,tool,palette,background,foreground,file'/>
           <link rel='canonical' href='https://vimcolors.org' />
+          <meta property="og:title" content="Vim colors generator" />
+          <meta property="og:site_name" content="vimcolors" />
+          <meta property="og:url" content="https://vimcolors.org" />
+          <meta property="og:description" content="Generate your own vim colorschemes and export them into a file" />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content="https://files-ev2m8ckfp.now.sh/" />
         </Head>
         <body>
           <Main />
