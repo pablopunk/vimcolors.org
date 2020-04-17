7b193f223a72e16c88169fa27b4671452f4ff1d4
pablopunk <pablovarela182@gmail.com>
2020-04-17 14:11:25 +0200

    Examples

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Terminal from '../components/terminal'
 export default () => {
   const [theme, setTheme] = useState('dark')
 
-  function toggleTheme () {
+  function toggleTheme() {
     if (theme === 'dark') {
       setTheme('light')
     } else {
@@ -13,42 +13,56 @@ export default () => {
     }
   }
 
-  return <div>
-    <div className='container'>
-      <ForkMe
-        position='right'
-        color='black'
-        href='//github.com/pablopunk/vimcolors.org'
-        target='_blank'
-      >
+  return (
+    <div>
+      <div className="container">
+        <ForkMe
+          position="right"
+          color="black"
+          href="//github.com/pablopunk/vimcolors.org"
+          target="_blank"
+        >
           Source on GitHub
-      </ForkMe>
-      <a href='/'>
-        <h1>Vim Colors</h1>
-      </a>
-      <small>`Generate your own vim colorschemes`</small>
-      <article>
-        <h2>1. </h2>
+        </ForkMe>
+        <a href="/">
+          <h1>Vim Colors</h1>
+        </a>
+        <small>`Generate your own vim colorschemes`</small>
+        <article>
+          <h2>1. </h2>
+          <p>
+            Choose below your favorite colors and they will become a vim
+            colorscheme!
+          </p>
+        </article>
+        <article>
+          <button onClick={() => toggleTheme()}>Toggle dark/light</button>
+        </article>
+        <Terminal theme={theme} />
+        <article>
+          <h2>2. </h2>
+          <p>
+            Now you can <code>:source 'path/to/your/file.vim'</code> inside vim
+            or neovim.
+          </p>
+        </article>
         <p>
-            Choose below your favorite colors and they
-            will become a vim colorscheme!
+          Alternatively, you can publish it via github and use it as any other
+          plugin. Checkout the examples below.
         </p>
-      </article>
-      <article>
-        <button onClick={() => toggleTheme()}>
-          Toggle dark/light
-        </button>
-      </article>
-      <Terminal theme={theme}/>
-      <article>
-        <h2>2. </h2>
-        <p>
-            Now you can <code>:source 'path/to/your/file.vim'</code> inside vim or neovim.
-        </p>
-      </article>
-      <p>Alternatively, you can publish it via github and use it as any other plugin. Check out <a href='//github.com/pablopunk/sick.vim'>this one</a> as a simple example.</p>
-    </div>
-    <style jsx>{`
+      </div>
+      <section>
+        <h3>Some examples of generated schemes</h3>
+        <ul>
+          <li>
+            <a href="https://github.com/pablopunk/sick.vim">sick.vim</a>
+          </li>
+          <li>
+            <a href="https://github.com/pablopunk/sunset.vim">sunset.vim</a>
+          </li>
+        </ul>
+      </section>
+      <style jsx>{`
         .container {
           display: flex;
           flex-direction: column;
@@ -61,7 +75,8 @@ export default () => {
         h2 {
           color: royalblue;
         }
-        h2, p {
+        h2,
+        p {
           display: inline;
         }
         article {
@@ -83,7 +98,7 @@ export default () => {
         button {
           margin: 1em;
           font-family: 'SF Mono', Menlo, monospace;
-          padding: .5em 2em;
+          padding: 0.5em 2em;
           border-radius: 3px;
           background-color: white;
           border: 1px solid gray;
@@ -93,5 +108,6 @@ export default () => {
           cursor: pointer;
         }
       `}</style>
-  </div>
+    </div>
+  )
 }
--- readme.md
+++ readme.md
@@ -4,6 +4,14 @@
 
 ![website](https://github.com/pablopunk/art/raw/master/vimcolors.org/vimcolors.org.png)
 
+### Example schemes
+
+These schemes were generated with this tool.
+
+- [sick.vim](https://github.com/pablopunk/sick.vim)
+- [sunset.vim](https://github.com/pablopunk/sunset.vim)
+- Add yours here!
+
 ### See also
 
 - [vim-colors](https://github.com/pablopunk/vim-colors): The npm module that this website uses
