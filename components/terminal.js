dc2d4c56e5ee7baa141003cda7f651ed7133edde
pablopunk <pablovarela182@gmail.com>
2020-04-19 19:31:49 +0200

    .js

--- components/terminal.js
+++ components/terminal.js
@@ -244,7 +244,7 @@ export default (props) => {
               backgroundColor: colors.menus,
             }}
           >
-            <span>accent/color</span>
+            <span>accent/color.js</span>
             {pickers.menus && (
               <div
                 className="picker"
