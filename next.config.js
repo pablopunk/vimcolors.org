54647ba3b5823502f3976bf026f4d9fe8c909120
Pablo P Varela <pablo@pablopunk.com>
2023-10-21 08:46:30 +0200

    update nextjs

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
+  compiler: {
+    styledComponents: true,
+  },
   async rewrites() {
     return [
       {
-        source: '/count.js',
-        destination: 'https://gc.zgo.at/count.js',
+        source: "/count.js",
+        destination: "https://gc.zgo.at/count.js",
       },
       {
-        source: '/goat',
-        destination: 'https://vimcolors.goatcounter.com/count',
+        source: "/goat",
+        destination: "https://vimcolors.goatcounter.com/count",
       },
-    ]
+    ];
   },
-}
+};
